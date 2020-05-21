47f9c5f179d51b79c6d7af4f9400c8fbd867e3f6
Brandon sellam <brandon.sellam@gmail.com>
2020-05-21 07:30:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 mongoose.connect("mongodb://devTrack:Blaugranna-19@ds161794.mlab.com:61794/heroku_40x0ptdl", {
   useNewUrlParser: true,
   useFindAndModify: false
